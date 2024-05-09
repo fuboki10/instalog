aa5f0f95a71f9ee302ee0c06db234ab9f18b6266
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-09 23:52:28 +0300

    Add dummy events route in the server

---
+++ server/api/events/index.ts
@@ -0,0 +1,7 @@
+import router from './router'
+
+export * from './router';
+
+export default {
+  router
+};
--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
+import events from './events';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/events', events.router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
