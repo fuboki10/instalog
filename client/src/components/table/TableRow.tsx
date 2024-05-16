7ecdadeff33b6c88f7e565ea596ac36f69c8edd3
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-16 13:14:03 +0300

    Dynamically map name to a gradient circle

--- client/src/components/table/TableRow.tsx
+++ client/src/components/table/TableRow.tsx
@@ -4,6 +4,7 @@ import Hidden from '../utils/Hidden';
 import formatDateToCustomFormat from '../../utils/format';
 import useOutsideClick from '../../utils/useOutsideClick';
 import useKeyDown from '../../utils/useKeyDown';
+import { NAME_GRADIENT_MAP } from './constants';
 
 interface TableRowProps {
   event: Event;
@@ -14,10 +15,21 @@ interface ViewProps {
   onClick: () => void;
 }
 
+const mapNameToGradient = (name: string) => {
+  const key = name.toLowerCase();
+  return Object.prototype.hasOwnProperty.call(NAME_GRADIENT_MAP, key)
+    ? NAME_GRADIENT_MAP[key]
+    : NAME_GRADIENT_MAP.default;
+};
+
 const RowView: React.FC<ViewProps> = ({ event, onClick }) => (
   <tr key={event.id}>
     <td className='flex gap-3 items-center px-6 py-4 text-start whitespace-nowrap'>
-      <div className='flex items-center justify-center h-7 w-7 text-center text-white font-bold text-sm rounded-full bg-gradient-to-r from-fuchsia-500 to-cyan-500'>
+      <div
+        className={`flex items-center justify-center h-7 w-7 text-center text-white font-bold text-sm rounded-full bg-gradient-to-tr ${mapNameToGradient(
+          event.actor_name
+        )}`}
+      >
         {event.actor_name[0]}
       </div>
       {event.target_name}
