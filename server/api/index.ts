fa9b176d15c31956798b615dcc7a4ff2a4c9a7c1
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-16 11:00:14 +0300

    Restructure files for deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,21 +1,3 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import cors from 'cors';
-import events from './events';
+import app from '../src';
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use('/events', events.router);
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+export default app;
